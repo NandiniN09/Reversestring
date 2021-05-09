a8a6e71bd33803ba4943f19e768516c2bd5cdaaf
Nandini <nandininarayanan96@gmail.com>
2021-05-09 19:48:33 -0400

    Add project files.

---
+++ Reversestring/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Reversestring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string str = "krishna";
+            var chars = str.ToCharArray();
+            var length = chars.Length;
+            Console.WriteLine($"string length is {length}");
+            for(var i=length-1; i>=0; i--)
+            {
+                Console.Write(chars[i]);
+            }
+        }
+    }
+}
